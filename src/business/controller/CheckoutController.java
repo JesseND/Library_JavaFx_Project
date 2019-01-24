d587cccedcbd62c42d12c4f9a9113ee588e53fad
Abdouraheem <46719420+Abdouraheem@users.noreply.github.com>
2019-01-24 11:23:42 -0600

    close window in checkout UI

--- src/business/controller/CheckoutController.java
+++ src/business/controller/CheckoutController.java
@@ -1,5 +1,6 @@
 package business.controller;
 
+import java.io.IOException;
 import java.lang.reflect.Member;
 import java.net.URL;
 import java.util.ArrayList;
@@ -12,12 +13,20 @@ import dataaccess.dao.PersonDAO;
 import dataaccess.tables.CheckoutTable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class CheckoutController implements Initializable {
 
@@ -38,6 +47,26 @@ public class CheckoutController implements Initializable {
 		for (Person p : members) {
 			membrsCbx.getItems().add(p.toString());
 		}
+	}
+	
+	@FXML
+	public void close(ActionEvent event) {
+		
+		((Node) event.getSource()).getScene().getWindow().hide();
+		Stage primaryStage = new Stage();
+		Parent root = null;
+		try {
+			root = FXMLLoader.load(getClass().getResource("/ui/MenuUI.fxml"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Scene scene = new Scene(root);
+		scene.setFill(Color.TRANSPARENT);
+
+		primaryStage.setScene(scene);
+		primaryStage.initStyle(StageStyle.TRANSPARENT);
+		primaryStage.show();
+		
 	}
 
 }
